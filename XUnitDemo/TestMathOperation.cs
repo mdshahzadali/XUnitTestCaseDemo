a092fe0d434a71a22f7d30639a9eac7970931cf8
Md Shahzad Ali <47299171+mdshahzadali@users.noreply.github.com>
2021-09-05 23:06:16 +0530

    Add files via upload

---
+++ XUnitDemo/TestMathOperation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using XunitConsoleApp;
+
+namespace XUnitTestDemo
+{
+    public class TestMathOperation
+    {
+        
+        [Fact]
+        public void TestAdditionalFunctionality_ShouldReturn20()
+        {
+            MathOperation mathOperation = new MathOperation();
+            int result = mathOperation.AddNumber(10, 10);
+            Assert.Equal(20, result);
+        }
+        [Fact]
+        public void TestAdditionalFunctionality_ShouldReturn5()
+        {
+            MathOperation mathOperation = new MathOperation();
+            int result = mathOperation.AddNumber(10, -5);
+            Assert.Equal(5, result);
+        }
+        [Fact]
+        public void TestAdditionalFunctionality_ShouldReturnNegative10()
+        {
+            MathOperation mathOperation = new MathOperation();
+            int result = mathOperation.AddNumber(-5, -5);
+            Assert.Equal(-10, result);
+        }
+        [Fact]
+        public void TestAdditionalFunctionality_ShouldReturn0()
+        {
+            MathOperation mathOperation = new MathOperation();
+            int result = mathOperation.AddNumber(-5, 5);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void TestMultiplicationFunctionality_ShouldReturn20()
+        {
+            MathOperation mathOperation = new MathOperation();
+            int result = mathOperation.MultiplyNumber(10, 2);
+            Assert.Equal(20, result);
+        }
+    }
+}
---
+++ XUnitDemo/TestStringRelatedProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using XunitConsoleApp;
+
+namespace XUnitTestDemo
+{
+    public class TestStringRelatedProgram
+    {
+        [Fact]
+        public void ShouldReturnReverseOfGivenString()
+        {
+            StringRelatedProgram stringRelatedProgram = new StringRelatedProgram();
+           string result = stringRelatedProgram.ReverseString("Hello");
+
+            Assert.Equal("olleH", result);
+        }
+        [Fact]
+        public void ShouldReturnTrueIfPassedStringPalindrome()
+        {
+            StringRelatedProgram stringRelatedProgram = new StringRelatedProgram();
+            bool result = stringRelatedProgram.checkIsPalindrome("madam");
+
+            Assert.True(result);
+        }
+        [Fact]
+        public void ShouldReturnFalseIfPassedStringNotPalindrome()
+        {
+            StringRelatedProgram stringRelatedProgram = new StringRelatedProgram();
+            bool result = stringRelatedProgram.checkIsPalindrome("satish");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldReverseWordOrder()
+        {
+            StringRelatedProgram stringRelatedProgram = new StringRelatedProgram();
+            string result = stringRelatedProgram.ReverseWordOrder("satish kumar");
+
+            Assert.Equal("kumar satish",result);
+        }
+    }
+}
